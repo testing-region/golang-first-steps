cf2687fc545936ab34447286690ec2e3fbf97073
David Saah <dasorange.hope@gmail.com>
2022-12-18 22:22:48 +0000

    create hello world program

---
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/01_hello/go.mod
@@ -0,0 +1,3 @@
+module github.com/Davesaah/go_crash_course/01_hello
+
+go 1.19
---
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/01_hello/main.go
@@ -0,0 +1,9 @@
+// To create the main function, call package main and create a main function
+
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello World")
+}

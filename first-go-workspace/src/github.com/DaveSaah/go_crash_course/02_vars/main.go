9f7b58904dd13f19ce870b4377db17ff0d9ba599
David Saah <dasorange.hope@gmail.com>
2022-12-20 15:01:03 +0000

    testing type conversion

--- first-go-workspace/src/github.com/DaveSaah/go_crash_course/02_vars/main.go
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/02_vars/main.go
@@ -1,9 +1,27 @@
 package main
 
-import "fmt"
-
-var num int = 42
+import (
+    "fmt"
+    "strconv"
+)
 
 func main() {
-    fmt.Println(num)
+    var num int = 45
+    fmt.Printf("%v, %T\n", num, num)
+
+    var num1 float32
+    num1 = float32(num)
+    fmt.Printf("%v, %T\n", num1, num1)
+
+    var num2 float32 = 42.5
+    fmt.Printf("%v, %T\n", num2, num2)
+
+
+    var num3 int
+    num3 = int(num2)
+    fmt.Printf("%v, %T\n", num3, num3)
+
+    var str string
+    str = strconv.Itoa(num)	// Itoa converts from an integer into an ascii string
+    fmt.Printf("%v, %T\n", str, str)
 }

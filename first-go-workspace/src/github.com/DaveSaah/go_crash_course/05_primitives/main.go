96c77e84216cd8d0d10f0732f3c528d172afe252
David Saah <dasorange.hope@gmail.com>
2022-12-20 18:20:29 +0000

    a look at runes

--- first-go-workspace/src/github.com/DaveSaah/go_crash_course/05_primitives/main.go
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/05_primitives/main.go
@@ -3,15 +3,6 @@ package main
 import "fmt"
 
 func main() {
-    var name string = "DaveSaah"
-    fmt.Println("I am", name)
-    fmt.Printf("%v, %T\n", name[2], name[2])
-    fmt.Printf("%v, %T\n", string(name[2]), name[2])
-
-    // string concatenation
-    fmt.Println(name + name)
-
-    // strings can be represented as a collection of bytes
-    bytes := []byte(name)
-    fmt.Println(bytes)
+    var runeVar = 'r'
+    fmt.Printf("%v, %T\n", runeVar, runeVar)
 }

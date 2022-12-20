37538c6c7ca583331eb9ef18ad207dd1921c8424
David Saah <dasorange.hope@gmail.com>
2022-12-20 17:53:07 +0000

    exponential notation with floats

--- first-go-workspace/src/github.com/DaveSaah/go_crash_course/05_primitives/main.go
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/05_primitives/main.go
@@ -3,12 +3,5 @@ package main
 import "fmt"
 
 func main() {
-    a := 10
-    b := 3
-
-    fmt.Println(a + b)
-    fmt.Println(a - b)
-    fmt.Println(a * b)
-    fmt.Println(10.0 / 3)
-    fmt.Println(a % b)
+    fmt.Println(2.1e4)
 }

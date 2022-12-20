903b7fbdee7439dfdc87066ca13e2b1c7f7b129d
David Saah <dasorange.hope@gmail.com>
2022-12-20 18:03:37 +0000

    working with complex numbers

--- first-go-workspace/src/github.com/DaveSaah/go_crash_course/05_primitives/main.go
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/05_primitives/main.go
@@ -3,5 +3,16 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println(2.1e4)
+    var comp complex64 = 1 + 10i
+    fmt.Println(comp + comp)
+    fmt.Println(comp - comp)
+    fmt.Println(comp * comp)
+    fmt.Println(comp / comp)
+
+    // To get the complex and imaginary parts separately
+    fmt.Println(real(comp), imag(comp))
+
+    // Create a complex number using the complex function
+    comp1 := complex(2, 3)
+    fmt.Println(comp1)
 }

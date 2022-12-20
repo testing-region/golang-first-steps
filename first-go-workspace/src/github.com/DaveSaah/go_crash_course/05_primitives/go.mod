171201d4540272c0330b2c0dd097dcfece76d851
David Saah <dasorange.hope@gmail.com>
2022-12-20 16:37:30 +0000

    add primitive type tests

---
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/05_primitives/go.mod
@@ -0,0 +1,3 @@
+module github.com/DaveSaah/golang-first-steps/05_primitives
+
+go 1.19
---
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/05_primitives/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    var isTrue = true
+    var isFalse = false
+    fmt.Println(isTrue)
+    fmt.Println(isFalse)
+    fmt.Println(1 == 1)
+
+    var isWhat string
+    fmt.Println(isWhat)
+
+    var num uint = 2
+    fmt.Println(num)
+}

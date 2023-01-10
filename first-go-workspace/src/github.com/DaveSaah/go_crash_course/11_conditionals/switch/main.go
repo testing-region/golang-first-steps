25ff18a9581bfddbd1864d0f82fd889c11bc788d
David Saah <dasorange.hope@gmail.com>
2023-01-10 17:11:54 +0000

    test switch statement with new values

--- first-go-workspace/src/github.com/DaveSaah/go_crash_course/11_conditionals/switch/main.go
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/11_conditionals/switch/main.go
@@ -21,22 +21,24 @@ func main() {
 
     // fallthrough
     switch {
-    case 5 > 10:
+    case 5 < 10:
 	fmt.Println("less than 10")
 	fallthrough
-    case 5 <= 20:
-	fmt.Println("less than 20")
+    case 5 == 20:
+	fmt.Println("equal to 20")
     }
 
     // type switches
-    var i interface{} = 1
+    var i interface{} = map[string] int{}
 
     switch i.(type) {
     case int:
 	fmt.Println("This is an integer")
     case string:
 	fmt.Println("This is a string")
+    case rune:
+	fmt.Println("This is a rune")
     default:
-	fmt.Println("This is another type")
+	fmt.Printf("Value is %T\n", i)
     }
 }

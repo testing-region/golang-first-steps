3de976d91dcfd6504ab97da7f60c6e15cf5bdea1
David Saah <dasorange.hope@gmail.com>
2023-02-21 23:31:15 +0000

    format

--- first-go-workspace/src/github.com/DaveSaah/go_crash_course/04_functions/main.go
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/04_functions/main.go
@@ -1,19 +1,16 @@
 package main
 
-
 import "fmt"
 
-
 func greeting(name string) string {
-    return "Hello " + name
+	return "Hello " + name
 }
 
 func getSum(num1, num2, num3 int) int {
-    return num1 + num2 + num3
+	return num1 + num2 + num3
 }
 
-
 func main() {
-    fmt.Println(greeting("David"))
-    fmt.Println(getSum(3, 9, 6))
+	fmt.Println(greeting("David"))
+	fmt.Println(getSum(3, 9, 6))
 }

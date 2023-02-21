0c97be2e41c117ea88f03de285eecbf426d834eb
David Saah <dasorange.hope@gmail.com>
2023-02-21 23:52:31 +0000

    add pointers notes

--- first-go-workspace/src/github.com/DaveSaah/go_crash_course/16_pointers/main.go
+++ first-go-workspace/src/github.com/DaveSaah/go_crash_course/16_pointers/main.go
@@ -2,28 +2,9 @@ package main
 
 import "fmt"
 
-type myStruct struct {
-    some int
-}
-
 func main() {
     var a int = 42
     var b *int = &a
+    a = 23
     fmt.Println(a, *b)
-    fmt.Println(&a, b)
-
-    a = 24
-    fmt.Println(a, *b)
-
-    *b = 19
-    fmt.Println(a, *b)
-
-    d := [3]int {1, 2, 3}
-    e := &d[0]
-    f := &d[1]
-    fmt.Printf("%v, %p, %p\n", d, e, f)
-
-    var ms *myStruct
-    ms = &myStruct{some: 42}
-    fmt.Println(*ms)
 }
